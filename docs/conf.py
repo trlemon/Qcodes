9887aef2cdcb52612317b0504cb4208383b03b2b
Jens H. Nielsen <jenielse@microsoft.com>
2026-03-16 09:36:03 +0100

    Fix intersphinx link to pandas

--- docs/conf.py
+++ docs/conf.py
@@ -435,7 +435,7 @@ texinfo_show_urls = "footnote"
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {
-    "pandas": ("https://pandas.pydata.org/pandas-docs/stable", None),
+    "pandas": ("https://pandas.pydata.org/docs", None),
     "matplotlib": ("https://matplotlib.org/stable", None),
     "python": ("https://docs.python.org/3", None),
     "numpy": ("https://numpy.org/doc/stable", None),
